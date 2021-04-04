097ac071aa4738c3ce95b8e5bf3166d317a72506
Bruno Ribeiro <brunorchaves1@gmail.com>
2021-04-04 18:50:48 -0300

    First implementation of funccional button event

--- pwm/pwm.ino
+++ pwm/pwm.ino
@@ -6,14 +6,15 @@ void setup() {
   
   pinMode(led_pin, OUTPUT);
   pinMode(button, INPUT);
+   Serial.begin(9600);
 }
 
 void loop() {
   int buttonState = 0;
   buttonState = digitalRead(button);
-
+  Serial.println(buttonState);
   machineLEds(buttonState);
-  machineServoMotor();
+  //machineServoMotor();
 }
 enum
 {
@@ -26,17 +27,25 @@ void machineLEds(int button)
   static int state = 0;
   static int32_t counter=0;
   static int direction = 0;
+  
   switch(state)
   {
     case State_readButton:
-      if(button !=1)
+      if(button == 0)
       {
         direction ^=1;
-        if(direction == 1)
+        if(direction == 1)       
+        {
           state = State_Rising;
+          counter = 1;
+        }
         else
+        {
           state = State_Fading;
+          counter = 255;
+        }
       }
+
       break;
     case State_Rising:
       if(counter >=254)
@@ -44,6 +53,7 @@ void machineLEds(int button)
         state = State_readButton;
       }
       counter++;
+      analogWrite(led_pin, counter);
       delay(5);
       break;
     case State_Fading:
@@ -53,11 +63,12 @@ void machineLEds(int button)
         state = State_readButton;
       }
       counter--;
+      analogWrite(led_pin, counter);
       delay(5);
       break;
     default:
       break;
   }
-  analogWrite(led_pin, counter);
+  
   
 }
